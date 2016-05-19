bea2ae0043b5259b9f6abca77748da431900ad69
AlexanderIB31 <aleks_bales@mail.ru>
2016-05-20 02:53:10 +0300

    Dropped semaphores

--- courseProject/test.cl
+++ courseProject/test.cl
@@ -1,14 +1,85 @@
-kernel void MultiplicationMatrix(global float* mA,
-								 global float* mB,
-								 global float* mC, 
-								 private unsigned int sz)
+#pragma OPENCL EXTENSION cl_khr_global_int32_base_atomics : enable
+
+kernel void MultMatrix(global float* mA,
+                       global float* mB,
+                       global float* mC,
+                       private unsigned int sz)
 {
-	int row = get_global_id(0);
-	int col = get_global_id(1);
-	float temp = 0;
-	for (int i = 0; i < sz; ++i) {
-		temp += mA[row * sz + i] * mB[i * sz + col];
-	}
-	mC[row * sz + col] = temp;
+    int row = get_global_id(0);
+    int col = row - row / sz * sz;
+    row /= sz;
+    float temp = 0;
+    for (int k = 0; k < sz; ++k)
+    {
+        temp += mA[row * sz + k] * mB[k * sz + col];
+    }
+    mC[row * sz + col] = temp;
 }
 
+kernel void FindMaxABS(global float* a, 
+					   global float* maxElem, 
+					   global int* posMax,
+					   private unsigned int sz)
+{
+    int pos = get_global_id(0);
+    int col = 0;
+    int row = pos * sz;
+    float _max = -1;
+	int posI, posJ;
+    for (int i = 0; i < sz; ++i) {
+        if (i == pos) continue;
+        if (_max < fabs(a[row + i])) {
+            _max = fabs(a[row + i]);
+			posJ = i;
+        }
+    }
+    maxElem[pos] = _max;
+    posMax[pos] = posJ;
+    barrier(CLK_GLOBAL_MEM_FENCE);
+    if (!pos) {
+    	_max = -1;
+    	posJ = -1;
+    	for (int i = 0; i < sz; ++i) {
+    		if (_max < maxElem[i]) {
+    			_max = maxElem[i];
+    			posI = i;
+    			posJ = posMax[i];
+    		}
+    	}
+    	posMax[0] = posI;
+    	posMax[1] = posJ;
+    }
+}
+
+kernel void ComputeNorm(global float* a,
+                        global float* res,
+			      		private unsigned int sz)
+{
+    int pos = get_global_id(0);
+    int row = pos * sz;
+    float sum = 0;
+    for (int i = 0; i < sz; ++i) {
+        if (pos == i) continue;
+        sum += a[row + i] * a[row + i];
+    }
+    res[pos] = sum;
+    barrier(CLK_GLOBAL_MEM_FENCE);
+    if (!pos) {
+    	sum = 0;
+    	for (int i = 0; i < sz; ++i) {
+    		sum += res[i];
+    	}
+    	res[0] = sum;
+    }
+}
+
+kernel void TransposeMatrix(global float* a,
+                            global float* res,
+                            private unsigned int sz)
+{
+    int pos = get_global_id(0);
+    int row = pos * sz;
+    for (int i = 0; i < sz; ++i) {
+        res[pos + i * sz] = a[row + i]; 
+    }
+}
--- courseProject/testProg.cpp
+++ courseProject/testProg.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
+#include <cmath>
 #include <cstdio>
 #include <unistd.h>
 #include <chrono>
+#include <cstring>
 
 #define MAX_SOURCE_SIZE (0x100000)
 
@@ -17,8 +19,36 @@ void checkRet(const cl_int err, const int raw) {
     }
 }
 
+cl_float Off(cl_float* matrix, cl_uint szMatrix, cl_kernel kernel,
+             cl_command_queue command_queue, cl_context context, size_t* global_work_size) {
+    cl_int err;
+    szMatrix = max(1u, szMatrix);
+    cl_float* norms     = (cl_float*)malloc(sizeof(cl_float) * szMatrix);
+    cl_mem buffMatrix = clCreateBuffer(context, CL_MEM_READ_ONLY, szMatrix * szMatrix * sizeof(cl_float), NULL, &err);
+    checkRet(err, __LINE__);
+    cl_mem buffNorm = clCreateBuffer(context, CL_MEM_READ_WRITE, szMatrix * sizeof(cl_float), NULL, &err);
+    checkRet(err, __LINE__);    
+    err = clEnqueueWriteBuffer(command_queue, buffMatrix, CL_FALSE, 0, szMatrix * szMatrix * sizeof(cl_float), matrix, 0, NULL, NULL);        
+    checkRet(err, __LINE__);
+    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&buffMatrix);
+    checkRet(err, __LINE__);
+    err = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&buffNorm);
+    checkRet(err, __LINE__);
+    err = clSetKernelArg(kernel, 2, sizeof(cl_uint), (void *)&szMatrix);
+    checkRet(err, __LINE__);    
+    err = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, global_work_size, NULL, 0, NULL, NULL);
+    checkRet(err, __LINE__);
+    err = clEnqueueReadBuffer(command_queue, buffNorm, CL_TRUE, 0, szMatrix * sizeof(cl_float), norms, 0, NULL, NULL);
+    checkRet(err, __LINE__);
+    clReleaseMemObject(buffMatrix);    
+    clReleaseMemObject(buffNorm);
+    cl_float norm = norms[0];
+    free(norms); 
+    return sqrt(norm);   
+}
+
 void RotateMethodGPU() {
-        string plotFileGPU = "plotDataGPU";
+    string plotFileGPU = "plotDataGPU";
     // инициализация устройств
     // ************************************************************************************
     cl_int              err;
@@ -28,9 +58,13 @@ void RotateMethodGPU() {
     cl_uint             num_devices;
     cl_context          context;
     cl_command_queue    command_queue;
-    cl_program          program     = NULL;
-    cl_kernel           kernelMultMatrix      = NULL;
+    cl_program          program                     = NULL;
+    cl_kernel           kernelFindMaxAbs            = NULL;
+    cl_kernel           kernelMultMatrix            = NULL;
+    cl_kernel           kernelComputeNorm           = NULL;
+    cl_kernel           kernelTranspose             = NULL;
     cl_context_properties platforms_properties[3];
+
     /* получить доступные платформы */
     err = clGetPlatformIDs(0, NULL, &num_platforms);
     checkRet(err, __LINE__);
@@ -81,7 +115,6 @@ void RotateMethodGPU() {
     // 3-ий аргумен = 0, т.к. всего одно устройство
     context = clCreateContextFromType(platforms_properties, CL_DEVICE_TYPE_GPU, NULL, NULL, &err);    
     checkRet(err, __LINE__);
-
     command_queue = clCreateCommandQueue(context, devices[0], 0, &err);
     checkRet(err, __LINE__);
 
@@ -109,7 +142,7 @@ void RotateMethodGPU() {
     checkRet(err, __LINE__);    
     /* скомпилировать программу */    
     err = clBuildProgram(program, 1, devices, NULL, NULL, NULL);
-        // build failed
+    // build failed
     if (err != CL_SUCCESS) {
         cl_build_status status;
         size_t logSize;
@@ -128,65 +161,205 @@ void RotateMethodGPU() {
         free(programLog);
     }
     // ************************************************************************************
-
-
+    cl_float EPS = 0.01;
+    cl_float EPSk;
+    
     /*  создать кернел */
-    kernelMultMatrix = clCreateKernel(program, "MultiplicationMatrix", &err);
+    kernelMultMatrix = clCreateKernel(program, "MultMatrix", &err);
     checkRet(err, __LINE__);
+    kernelFindMaxAbs = clCreateKernel(program, "FindMaxABS", &err);
+    checkRet(err, __LINE__);
+    kernelMultMatrix = clCreateKernel(program, "MultMatrix", &err);
+    checkRet(err, __LINE__);
+    kernelComputeNorm = clCreateKernel(program, "ComputeNorm", &err);
+    checkRet(err, __LINE__);
+    kernelTranspose = clCreateKernel(program, "TransposeMatrix", &err);
+    checkRet(err, __LINE__);
+
+    cl_float*   matrixA         = NULL;
+    cl_float*   matrixB         = NULL;
+    cl_float*   matrixC         = NULL;
+    cl_float*   matrixRotate    = NULL;
+    cl_float*   matrixRotateT   = NULL;
     
     ofstream out(plotFileGPU, ios::out);
-    for (size_t cnt = 10; cnt < 800; cnt += 50) {
-        //size_t cnt = atoi(argv[1]);
-        size_t szMatrix = cnt;
+    for (cl_uint cnt = 3; cnt <= 3; cnt += 50) {
+        cl_uint szMatrix = cnt;
         size_t global_work_size[2] = { szMatrix, szMatrix };
-        cl_mem      memobjMatrixA      = NULL;
-        cl_mem      memobjMatrixB      = NULL;
-        cl_mem      memobjMatrixC      = NULL;
+
         // квадратная матрица, симметричная
-        cl_float*   matrix = (cl_float*)malloc(sizeof(cl_float) * szMatrix * szMatrix);
-        for (size_t i = 0; i < szMatrix; ++i) {
-            for (size_t j = i; j < szMatrix; ++j) {
-                matrix[i * szMatrix + j] = matrix[j * szMatrix + i] = i * szMatrix + j + 1;
+        matrixA         = (cl_float*)malloc(sizeof(cl_float) * szMatrix * szMatrix);
+        matrixB         = (cl_float*)malloc(sizeof(cl_float) * szMatrix * szMatrix);
+        matrixC         = (cl_float*)malloc(sizeof(cl_float) * szMatrix * szMatrix);
+        matrixRotate    = (cl_float*)malloc(sizeof(cl_float) * szMatrix * szMatrix);
+        matrixRotateT   = (cl_float*)malloc(sizeof(cl_float) * szMatrix * szMatrix);        
+
+        cl_mem      buffMatrixA      = NULL;
+        cl_mem      buffMatrixB      = NULL;
+        cl_mem      buffMatrixC      = NULL;
+
+        for (cl_uint i = 0; i < szMatrix; ++i) {
+            for (cl_uint j = 0; j < szMatrix; ++j) {
+                if (j > i) {
+                    matrixA[i * szMatrix + j] = matrixA[j * szMatrix + i] = i * szMatrix + j + 1;
+                }
+                matrixRotate[i * szMatrix + j] = matrixRotateT[i * szMatrix + j] =
+                    matrixB[i * szMatrix + j] = matrixC[i * szMatrix + j] = 0;
             }
         }
-        /* создать буфер */
-        memobjMatrixA  = clCreateBuffer(context, CL_MEM_READ_ONLY, szMatrix * szMatrix * sizeof(cl_float), NULL, &err);
-        checkRet(err, __LINE__);    
-        memobjMatrixB  = clCreateBuffer(context, CL_MEM_READ_ONLY, szMatrix * szMatrix * sizeof(cl_float), NULL, &err);
-        checkRet(err, __LINE__);    
-        memobjMatrixC  = clCreateBuffer(context, CL_MEM_WRITE_ONLY, szMatrix * szMatrix * sizeof(cl_float), NULL, &err);
-        checkRet(err, __LINE__);        
-        /* записать данные в буфер */
-        err = clEnqueueWriteBuffer(command_queue, memobjMatrixA, CL_FALSE, 0, szMatrix * szMatrix * sizeof(cl_float), matrix, 0, NULL, NULL);        
-        checkRet(err, __LINE__);
-        err = clEnqueueWriteBuffer(command_queue, memobjMatrixB, CL_FALSE, 0, szMatrix * szMatrix * sizeof(cl_float), matrix, 0, NULL, NULL);        
-        checkRet(err, __LINE__);
-        /* устанавливаем параметр (2-ой параметр отвечает за порядковый номер 4-ого аргумента в ф-ии __kernelMultMatrix */
-        err = clSetKernelArg(kernelMultMatrix, 0, sizeof(cl_mem), (void *)&memobjMatrixA);
-        checkRet(err, __LINE__);
-        err = clSetKernelArg(kernelMultMatrix, 1, sizeof(cl_mem), (void *)&memobjMatrixB);
-        checkRet(err, __LINE__);
-        err = clSetKernelArg(kernelMultMatrix, 2, sizeof(cl_mem), (void *)&memobjMatrixC);
-        checkRet(err, __LINE__);
-        err = clSetKernelArg(kernelMultMatrix, 3, sizeof(cl_uint), (void *)&szMatrix);
-        checkRet(err, __LINE__);
-        /* выполнить кернел */
-        //usleep(5);
-        auto start = chrono::system_clock::now();
-        err = clEnqueueNDRangeKernel(command_queue, kernelMultMatrix, 2, NULL, global_work_size, NULL, 0, NULL, NULL);
-        checkRet(err, __LINE__);
-
-        /* считать данные из буфера */
-        err = clEnqueueReadBuffer(command_queue, memobjMatrixC, CL_TRUE, 0, szMatrix * szMatrix * sizeof(cl_float), matrix, 0, NULL, NULL);
-        checkRet(err, __LINE__);
-        auto end = chrono::system_clock::now();
-        auto elapsed_ns = chrono::duration_cast<chrono::milliseconds>(end - start);
-        out << cnt << " " << elapsed_ns.count() << endl;
         
-        clReleaseMemObject(memobjMatrixA);
-        clReleaseMemObject(memobjMatrixB);
-        clReleaseMemObject(memobjMatrixC);
-        free(matrix);
+
+        EPSk = Off(matrixA, cnt, kernelComputeNorm, command_queue, context, global_work_size);
+        cout << "# " << EPSk << endl;
+        while (EPSk >= EPS) {
+            for (cl_uint k = 0; k < szMatrix; ++k) {
+                matrixRotate[k * szMatrix + k] = 1;
+            }
+            cl_float* maxElem   = (cl_float*)malloc(sizeof(cl_float) * szMatrix);
+            cl_int* maxPos      = (cl_int*)malloc(sizeof(cl_int) * max(1u * 2, szMatrix));
+            
+            cl_mem buffMaxPos       = NULL;
+            cl_mem buffMaxElem      = NULL;
+
+            buffMatrixA  = clCreateBuffer(context, CL_MEM_READ_ONLY, szMatrix * szMatrix * sizeof(cl_float), NULL, &err);
+            checkRet(err, __LINE__);    
+            buffMaxElem  = clCreateBuffer(context, CL_MEM_READ_WRITE, szMatrix * sizeof(cl_float), NULL, &err);
+            checkRet(err, __LINE__);    
+            buffMaxPos  = clCreateBuffer(context, CL_MEM_READ_WRITE, max(1u * 2, szMatrix) * sizeof(cl_int), NULL, &err);
+            checkRet(err, __LINE__);    
+            err = clEnqueueWriteBuffer(command_queue, buffMatrixA, CL_FALSE, 0, 
+                szMatrix * szMatrix * sizeof(cl_float), matrixA, 0, NULL, NULL);        
+            checkRet(err, __LINE__);
+            err = clSetKernelArg(kernelFindMaxAbs, 0, sizeof(cl_mem), (void *)&buffMatrixA);
+            checkRet(err, __LINE__);            
+            err = clSetKernelArg(kernelFindMaxAbs, 1, sizeof(cl_mem), (void *)&buffMaxElem);
+            checkRet(err, __LINE__);            
+            err = clSetKernelArg(kernelFindMaxAbs, 2, sizeof(cl_mem), (void *)&buffMaxPos);
+            checkRet(err, __LINE__);            
+            err = clSetKernelArg(kernelFindMaxAbs, 3, sizeof(cl_uint), (void *)&szMatrix);
+            checkRet(err, __LINE__);                      
+            err = clEnqueueNDRangeKernel(command_queue, kernelFindMaxAbs, 1, NULL, global_work_size, NULL, 0, NULL, NULL);
+            checkRet(err, __LINE__);
+            err = clEnqueueReadBuffer(command_queue, buffMaxPos, CL_TRUE, 0, max(1u * 2, szMatrix) * sizeof(cl_int), maxPos, 0, NULL, NULL);
+            checkRet(err, __LINE__);            
+            clReleaseMemObject(buffMatrixA);    
+            clReleaseMemObject(buffMaxElem);
+            clReleaseMemObject(buffMaxPos);
+
+            cl_int i = maxPos[0], j = maxPos[1];
+            free(maxElem);
+            free(maxPos);
+            cout << "# " << i << " | " << j << endl;
+            cl_float angel = fabs(matrixA[i * szMatrix + i] - matrixA[j * szMatrix + j])
+                < 0.001 * EPS ? M_PI / 4 :
+                (0.5 * atan(2.0 * matrixA[i * szMatrix + j]
+                    / (matrixA[i * szMatrix + i] - matrixA[j * szMatrix + j])));
+            matrixRotate[i * szMatrix + i] = matrixRotate[j * szMatrix + j] = cos(angel);
+            matrixRotate[i * szMatrix + j] = -(matrixRotate[j * szMatrix + i] = sin(angel));
+
+            for (size_t i = 0; i < szMatrix; ++i) {
+                for (size_t j = 0; j < szMatrix; ++j) {
+                    cout << matrixRotate[i * szMatrix + j] << " ";
+                }
+                cout << endl;
+            }
+
+
+            buffMatrixA  = clCreateBuffer(context, CL_MEM_READ_ONLY, szMatrix * szMatrix * sizeof(cl_float), NULL, &err);
+            checkRet(err, __LINE__);
+            buffMatrixB  = clCreateBuffer(context, CL_MEM_WRITE_ONLY, szMatrix * szMatrix * sizeof(cl_float), NULL, &err);
+            checkRet(err, __LINE__);
+            err = clEnqueueWriteBuffer(command_queue, buffMatrixA, CL_FALSE, 0, 
+                szMatrix * szMatrix * sizeof(cl_float), matrixRotate, 0, NULL, NULL);        
+            checkRet(err, __LINE__);
+            err = clSetKernelArg(kernelTranspose, 0, sizeof(cl_mem), (void *)&buffMatrixA);
+            checkRet(err, __LINE__);            
+            err = clSetKernelArg(kernelTranspose, 1, sizeof(cl_mem), (void *)&buffMatrixB);
+            checkRet(err, __LINE__);            
+            err = clSetKernelArg(kernelTranspose, 2, sizeof(cl_uint), (void *)&szMatrix);
+            checkRet(err, __LINE__);                        
+            err = clEnqueueNDRangeKernel(command_queue, kernelTranspose, 1, NULL, global_work_size, NULL, 0, NULL, NULL);
+            checkRet(err, __LINE__);
+            err = clEnqueueReadBuffer(command_queue, buffMatrixB, CL_TRUE, 0, 
+                szMatrix * szMatrix * sizeof(cl_float), matrixRotateT, 0, NULL, NULL);
+            checkRet(err, __LINE__);
+            clReleaseMemObject(buffMatrixA);
+            clReleaseMemObject(buffMatrixB);
+
+            for (size_t i = 0; i < szMatrix; ++i) {
+                for (size_t j = 0; j < szMatrix; ++j) {
+                    cout << matrixRotateT[i * szMatrix + j] << " ";
+                }
+                cout << endl;
+            }
+
+
+            buffMatrixA  = clCreateBuffer(context, CL_MEM_READ_ONLY, szMatrix * szMatrix * sizeof(cl_float), NULL, &err);
+            checkRet(err, __LINE__);
+            buffMatrixB  = clCreateBuffer(context, CL_MEM_READ_ONLY, szMatrix * szMatrix * sizeof(cl_float), NULL, &err);
+            checkRet(err, __LINE__);            
+            buffMatrixC  = clCreateBuffer(context, CL_MEM_WRITE_ONLY, szMatrix * szMatrix * sizeof(cl_float), NULL, &err);
+            checkRet(err, __LINE__);
+            err = clEnqueueWriteBuffer(command_queue, buffMatrixA, CL_FALSE, 0, 
+                szMatrix * szMatrix * sizeof(cl_float), matrixRotateT, 0, NULL, NULL);        
+            checkRet(err, __LINE__);
+            err = clEnqueueWriteBuffer(command_queue, buffMatrixB, CL_FALSE, 0, 
+                szMatrix * szMatrix * sizeof(cl_float), matrixA, 0, NULL, NULL);        
+            checkRet(err, __LINE__);            
+            err = clSetKernelArg(kernelMultMatrix, 0, sizeof(cl_mem), (void *)&buffMatrixA);
+            checkRet(err, __LINE__);            
+            err = clSetKernelArg(kernelMultMatrix, 1, sizeof(cl_mem), (void *)&buffMatrixB);
+            checkRet(err, __LINE__);            
+            err = clSetKernelArg(kernelMultMatrix, 2, sizeof(cl_mem), (void *)&buffMatrixC);
+            checkRet(err, __LINE__);                        
+            err = clSetKernelArg(kernelMultMatrix, 3, sizeof(cl_uint), (void *)&szMatrix);
+            checkRet(err, __LINE__);                                    
+            err = clEnqueueNDRangeKernel(command_queue, kernelMultMatrix, 2, NULL, global_work_size, NULL, 0, NULL, NULL);
+            checkRet(err, __LINE__);
+            err = clEnqueueReadBuffer(command_queue, buffMatrixC, CL_TRUE, 0, 
+                szMatrix * szMatrix * sizeof(cl_float), matrixC, 0, NULL, NULL);
+            checkRet(err, __LINE__);
+            
+            err = clEnqueueWriteBuffer(command_queue, buffMatrixA, CL_FALSE, 0, 
+                szMatrix * szMatrix * sizeof(cl_float), matrixC, 0, NULL, NULL);        
+            checkRet(err, __LINE__);
+            err = clEnqueueWriteBuffer(command_queue, buffMatrixB, CL_FALSE, 0, 
+                szMatrix * szMatrix * sizeof(cl_float), matrixRotate, 0, NULL, NULL);        
+            err = clSetKernelArg(kernelMultMatrix, 0, sizeof(cl_mem), (void *)&buffMatrixA);
+            checkRet(err, __LINE__);            
+            err = clSetKernelArg(kernelMultMatrix, 1, sizeof(cl_mem), (void *)&buffMatrixB);
+            checkRet(err, __LINE__);            
+            err = clSetKernelArg(kernelMultMatrix, 2, sizeof(cl_mem), (void *)&buffMatrixC);
+            checkRet(err, __LINE__);                        
+            err = clSetKernelArg(kernelMultMatrix, 3, sizeof(cl_uint), (void *)&szMatrix);
+            checkRet(err, __LINE__);                                    
+            err = clEnqueueNDRangeKernel(command_queue, kernelMultMatrix, 2, NULL, global_work_size, NULL, 0, NULL, NULL);
+            checkRet(err, __LINE__);
+            err = clEnqueueReadBuffer(command_queue, buffMatrixC, CL_TRUE, 0, 
+                szMatrix * szMatrix * sizeof(cl_float), matrixA, 0, NULL, NULL);
+            checkRet(err, __LINE__);
+
+            clReleaseMemObject(buffMatrixA);
+            clReleaseMemObject(buffMatrixB);
+            clReleaseMemObject(buffMatrixC);
+
+            EPSk = Off(matrixA, cnt, kernelComputeNorm, command_queue, context, global_work_size);
+            cout << "#: " << EPSk << endl;
+            for (size_t i = 0; i < szMatrix; ++i) {
+                for (size_t j = 0; j < szMatrix; ++j) {
+                    cout << matrixA[i * szMatrix + j] << " ";
+                }
+                cout << endl;
+            }
+
+        }
+
+
+        free(matrixA);
+        free(matrixB);
+        free(matrixC);
+        free(matrixRotate);
+        free(matrixRotateT);
     }
     out.close();   
     free(source_str);
@@ -232,6 +405,6 @@ void RotateMethodCPU() {
 
 int main(int argc, char* argv[]) {
     RotateMethodGPU();
-    RotateMethodCPU();
+    //RotateMethodCPU();
     return 0;
 }
